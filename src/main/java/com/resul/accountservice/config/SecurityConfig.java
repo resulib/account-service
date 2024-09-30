bcc5bfd0b7cf3b1dd5791e10706761d6172a2034
resulib <mibadullayev98@gmail.com>
2024-09-30 23:33:28 +0400

    refactoring

--- src/main/java/com/resul/accountservice/config/SecurityConfig.java
+++ src/main/java/com/resul/accountservice/config/SecurityConfig.java
@@ -23,7 +23,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers( "/api/v1/accounts/**","/swagger-ui/", "/swagger-ui/**", "/swagger-resources/**",
+                        .requestMatchers(
+//                                "/api/v1/accounts/**",
+                                "/swagger-ui/", "/swagger-ui/**", "/swagger-resources/**",
                                 "/configuration/ui", "/v3/api-docs/**", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
                 )
